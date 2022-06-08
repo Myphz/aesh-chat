abbe158f6f84d884d410ddcf8e7857644adec5c1
Myphz <danielscanu45@outlook.it>
2022-06-08 15:02:19 +0200

    feature: add `Message` component and styling

---
+++ src/components/Message.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="h5 message">
+    {{ text }}
+  </div>
+</template>
+
+<script setup>
+const { text } = defineProps({
+  text: {
+    type: String,
+    required: true
+  },
+
+  differentSender: {
+    type: Boolean,
+    required: true
+  }
+});
+</script>
+
+<style lang="sass" scoped>
+  .message
+    max-width: 100%
+    background-color: $accent
+    border-radius: .2em
+    overflow-wrap: break-word
+    padding: .5em 1em
+    margin: .3em
+
+</style>
--- src/components/Messages.vue
+++ src/components/Messages.vue
@@ -1,8 +1,16 @@
 <template>
-  <div>
-    test messages...
-  </div>
+  <section>
+    <Message v-for="i in 10" :key="i" text="test" />
+  </section>
 </template>
 
+<script setup>
+import Message from "@/components/Message.vue";
+</script>
+
 <style lang="sass" scoped>
+  section
+    width: 50vw
+    display: flex
+    flex-direction: column
 </style>
