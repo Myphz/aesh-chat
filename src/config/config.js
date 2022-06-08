d66765342c935a6f81f7c83b56245eb2bdc4225f
Myphz <danielscanu45@outlook.it>
2022-06-08 20:43:29 +0200

    fix: use wss instead of ws for production

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
 export const SERVER_URL = process.env.NODE_ENV !== "production" ? "ws://localhost:5000/messages" : (function() {
   const currentURL = window.location.origin;
-  return currentURL.replace(window.location.protocol, "ws:") + "/messages";
+  return currentURL.replace(window.location.protocol, "wss:") + "/messages";
 })();
